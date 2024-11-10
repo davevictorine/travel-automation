43976867b1d4dafefdc4f79ea5f870e6ca35442d
davevictorine <davidvictorine@MacBook-Pro.attlocal.net>
2024-11-10 17:13:14 -0600

    Thunder Client Started

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -73,62 +73,60 @@ def get_va_tasks():
 @app.route('/api/post-ideas', methods=["GET", "POST"]) # Test
 def get_post_ideas():
     if request.method == "POST":
-       new_idea = request.json
-       print(new_idea)
+        # Get the new idea from the request
+        new_idea = request.json
+        print("New idea received:", new_idea)
+        return jsonify({"message": "Received your post!"})
     else:
-        # Your existing GET logic should go here
-    
-    ideas = {
-        "post_ideas": [
-            {
-                "title": "Best Oklahoma Towns to Visit in December",
-                "keyword": "Best Places to Visit in December",
-                "kd": 3,
-                "volume": 1200
-            },
-            {
-                "title": "Where to See Christmas Lights in Tulsa",
-                "keyword": "Christmas Lights Near Me",
-                "kd": 4,
-                "volume": 34000
-            },
-            {
-                "title": "Christmas Events in Missouri",
-                "keyword": "Christmas Events Near Me",
-                "kd": 1,
-                "volume": 26000
-            }
+        ideas = {    # This line and everything after needs to be indented!
+            "post_ideas": [
+                {
+                    "title": "Best Oklahoma Towns to Visit in December",
+                    "keyword": "Best Places to Visit in December",
+                    "kd": 3,
+                    "volume": 1200
+                },
+                {
+                    "title": "Where to See Christmas Lights in Tulsa",
+                    "keyword": "Christmas Lights Near Me",
+                    "kd": 4,
+                    "volume": 34000
+                },
+                {
+                    "title": "Christmas Events in Missouri",
+                    "keyword": "Christmas Events Near Me",
+                    "kd": 1,
+                    "volume": 26000
+                }
+            ]
+        }
 
+        # Calculate total number of ideas
+        total_ideas = len(ideas["post_ideas"])
+        
+        # Initialize variables for calculations
+        total_kd = 0
+        total_volume = 0
+        
+        # Loop through ideas to sum up values
+        for idea in ideas["post_ideas"]:
+            total_kd += idea["kd"]
+            total_volume += idea["volume"]
+        
+        # Calculate averages
+        avg_kd = total_kd / total_ideas
 
-        ]
-    }
-    # Calculate total number of ideas
-    
-    total_ideas = len(ideas["post_ideas"])
+        # Create metrics dictionary
+        metrics = {
+            "total_ideas": total_ideas,
+            "average_kd": round(avg_kd, 1),
+            "total_search_volume": total_volume
+        }
 
-    total_kd = 0
-    total_volume = 0
+        # Add metrics to our response
+        ideas["metrics"] = metrics
 
-    # Loop through ideas
-    for idea in ideas["post_ideas"]:
-        total_kd +=idea["kd"]
-        total_volume += idea["volume"]
-
-    # Calculate averages
-    avg_kd = total_kd / total_ideas
-
-    # Create metrics dictionary
-
-    metrics = {
-        "total_ideas": total_ideas,
-        "average_kd": round(avg_kd, 1),
-        "toatl_search_volumd": total_volume
-    }
-
-# Add metrics to response
-    ideas["metrics"] = metrics
-
-    return jsonify(ideas)
+        return jsonify(ideas)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
